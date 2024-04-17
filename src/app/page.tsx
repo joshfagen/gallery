075d162762ae7a7a5c954cdff28861603c0ebdb5
Josh <joshmfagen@gmail.com>
2024-04-17 00:42:46 -0400

    creates images table and displays from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,20 +15,16 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts)
+  const images = await db.query.images.findMany();
+  console.log(images)
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {images.map((image, index) => (
           <div key={`${image.id}-${index}`} className="w-48">
-            <img src={image.url} />
+          <img src={image.url} />
 
-          </div>
+        </div>
         ))}
       </div>
     </main>
