9f73dfbfe32ef86242be9728f9726ff0c42875c8
Josh <joshmfagen@gmail.com>
2024-04-17 13:52:06 -0400

    use auth for homepage

--- src/app/_components/topNav.tsx
+++ src/app/_components/topNav.tsx
@@ -4,10 +4,12 @@ export function TopNav() {
     return (
       <nav className="flex items-center justify-between 
       w-full border-b p-4 text-xl ">
-        <div>Gallery</div>
+        <div className="text-yellow-200">Your Sites</div>
         <div>
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <button className="text-yellow-200">Sign-In</button>
+            </SignInButton>
           </SignedOut>
 
           <SignedIn>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,45 @@
 import Link from "next/link";
+import { SignedOut, SignedIn, SignInButton} from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
+const images = await db.query.images.findMany({
+  orderBy: (model, { desc }) => desc(model.id),
+});
+
+// async function Images() {
+//     {[...images, ...images, ...images].map((image, index) => (
+//       <div key={`${image.id}-${index}`} className="w-48 flex-col">
+//         <img src={image.url}/>
+//         <div className="text-yellow-200">{image.name.toUpperCase()}</div>
+
+//       </div>
+//     ))}
+// }
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-  console.log(images)
+
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
+      <SignedOut>
+        <div className="h-full w-full pt-16 text-2xl text-center">
+          Please Sign-In&nbsp; 
+          <SignInButton>
+            <button className='text-yellow-200 hover:underline'>Here</button>
+          </SignInButton>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images].map((image, index) => (
           <div key={`${image.id}-${index}`} className="w-48 flex-col">
-          <img src={image.url}/>
-          <div>{image.name.toUpperCase()}</div>
+            <img src={image.url}/>
+            <div className="text-yellow-200">{image.name.toUpperCase()}</div>
 
-        </div>
+          </div>
         ))}
-      </div>
+        </div>
+      </SignedIn>
     </main>
   );
 }
