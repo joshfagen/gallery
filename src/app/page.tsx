c3b62e85fb2d73ad3c710daac9ada5d48ec65fe0
Josh <joshmfagen@gmail.com>
2024-04-17 01:04:57 -0400

    images now displaying from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,19 @@ import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  'https://utfs.io/f/38b0c3e7-533d-44e2-8748-86cd1e699135-dayvvh.webp',
-  'https://utfs.io/f/5a6a549b-53f7-471d-a200-cd6194f81523-f687ea.jpg',
-  'https://utfs.io/f/94619253-13a6-45d8-9215-3cb99cb82c80-2bol.png',
-  'https://utfs.io/f/a3687fb7-7752-40f3-a137-faab2e60cac1-r6q6q0.jpg'
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   console.log(images)
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={`${image.id}-${index}`} className="w-48">
-          <img src={image.url} />
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={`${image.id}-${index}`} className="w-48 flex-col">
+          <img src={image.url}/>
+          <div>{image.name.toUpperCase()}</div>
 
         </div>
         ))}
